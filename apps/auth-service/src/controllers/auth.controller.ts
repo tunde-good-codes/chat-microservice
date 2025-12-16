3126437f8c2a121afef67cd5d33b678d3188e062
tundenewcodes <fasasipelumi1652@gmail.com>
2025-12-16 23:59:14 +0100

    error handling prisma

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -1,3 +1,4 @@
+import prisma from "@/database";
 import { AuthResponse, RegisterInput } from "@/types";
 import { ValidationError } from "@shared/error-handler";
 import { Request, Response, NextFunction } from "express";
@@ -10,6 +11,11 @@ const registerUser = async (
   const { email, displayName, password } = req.body as RegisterInput;
 
   if (!email || !displayName || !password) {
+    res.status(400).json({
+      success: false,
+      message: "all fields are required",
+    });
     throw new ValidationError("All the fields are required!");
   }
+  const existingEmail = await prisma.
 };
