956f688ff3c4e665ada8e7446816f112c4709aa6
tundenewcodes <fasasipelumi1652@gmail.com>
2025-12-16 23:55:57 +0100

    error handling

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -1,1 +1,15 @@
+import { AuthResponse, RegisterInput } from "@/types";
+import { ValidationError } from "@shared/error-handler";
+import { Request, Response, NextFunction } from "express";
 
+const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<AuthResponse> => {
+  const { email, displayName, password } = req.body as RegisterInput;
+
+  if (!email || !displayName || !password) {
+    throw new ValidationError("All the fields are required!");
+  }
+};
